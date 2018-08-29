5f29007e8f9b743d33205399416cf08893ac5175
areshet <a.reshetov94@gmail.com>
2018-08-29 18:15:44 +0500

    создание массива и запись в файл

---
+++ class/ArrayToFile.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class ArrayToFile
+{
+    public static function main()
+    {
+        $filename1 = 'array-1.json';
+        $filename2 = 'array-2.json';
+        $array = array();
+        for ($i = 0; $i < 100; $i++) {
+            $array[$i] = self::getRandom(0, 5685);
+        }
+        self::recordFile($array, $filename1);
+
+        for ($i = 1; $i < 100; $i++) {
+            if ($i % 4 == 0 || $i & 1) {
+                $array[$i] = ($array[$i] - 23) * 2;
+            }
+        }
+        $array2 = array_filter($array, function ($i) {
+            return $i >= 2450 && $i < 4031;
+        });
+        self::recordFile($array2, $filename2);
+
+    }
+
+    static function getRandom($min, $max)
+    {
+        return rand($min, $max);
+    }
+
+    static function recordFile($mass, $filename)
+    {
+        $json = json_encode($mass);
+        $fp = fopen($filename, "w");
+        fwrite($fp, $json);
+        fclose($fp);
+    }
+
+
+}
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'class/ArrayToFile.php';
+ArrayToFile::main();
+
